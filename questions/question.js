6252d19b5ad8664d6c7dc4cb793a1b8f575e4031
Ahmed Maher <ahmedmaheratiyah@gmail.com>
2020-12-16 10:53:59 +0200

    add promises to questions

--- questions/question.js
+++ questions/question.js
@@ -10,7 +10,7 @@ const findQuestionMongo = async (id) => {
 
 const findPGService = async (mongoService) => {
   const file = await fs.readFile('../services/pg-services.json');
-  const pgServices= JSON.parse(file);
+  const pgServices = JSON.parse(file);
 
   const service = pgServices.filter(
     (c) =>
@@ -33,124 +33,128 @@ const main = async () => {
 
   var questionTextCount = 1;
   var questionCount = 1;
-  
+
   Promise.all(
     dataServices.map(async (service, i) => {
-      if(service.spec_questions.length > 0){
-        service.spec_questions.map(async (questionId, index) => {
-          const item = await findQuestionMongo(service.spec_questions[index].$oid);
-          if(item !== undefined){
-        const servicePG = await findPGService(service);
-        var questionText;
-        
-  
-  
-        const question = {
-          id: questionCount,
-          type: "question",
-          skip: false,
-          input_bar: false,
-          client_only: false,
-          filter_show: item.filter,
-          pro_page:item.check,
-          is_template:false,
-          createdAt: '2020-12-11 01:23:53.941+02',
-          updatedAt: '2020-12-11 01:23:53.941+02',
-          serviceId: servicePG.id,
-        };
-  
-        //console.log(question);
-        questionCount++;
-        questions.push(question);
-        console.log(question);
-  
-  
-  
-        // Question Texts START
-  
-        //Pro Question text
-        questionText = {
-          id:questionTextCount,
-          uz:item.question.uz,
-          ru:item.question.ru,
-          eng:item.question.eng,
-          belongs:"pro",
-          group_id:0,
-          type:"question",
-          multiple:false,
-          createdAt: '2020-12-11 01:23:53.941+02',
-          updatedAt: '2020-12-11 01:23:53.941+02',
-          questionId:questionCount- 1,
-        };
-        questionTextCount++;
-        //console.log(questionText);
-        questionTextArray.push(questionText);
-  
-        //Client Question text
-        questionText = {
-          id:questionTextCount,
-          uz:item.title.uz,
-          ru:item.title.ru,
-          eng:item.title.eng,
-          belongs:"client",
-          group_id:0,
-          type:"question",
-          multiple:false,
-          createdAt: '2020-12-11 01:23:53.941+02',
-          updatedAt: '2020-12-11 01:23:53.941+02',
-          questionId:questionCount- 1,
-        };
-        questionTextCount++;
-        //console.log(questionText);
-        questionTextArray.push(questionText);
-  
-        // Choiceses QuestionText
-        
-        await item.values.map( (value, index) => {
-          questionText = {
-            id:questionTextCount,
-            uz:value.uz,
-            ru:value.ru,
-            eng:value.eng,
-            belongs:"client",
-            group_id:index,
-            type:"choice",
-            multiple:item.multiple,
-            createdAt: '2020-12-11 01:23:53.941+02',
-            updatedAt: '2020-12-11 01:23:53.941+02',
-            questionId:questionCount- 1,
-          };
-          questionTextCount++;
-          //console.log(questionText);
-          questionTextArray.push(questionText);
-   
-        })
-       
-        //Question Text END
-          
-        }
-        
-      })
-    }
+      if (service.spec_questions.length > 0) {
+        await Promise.all(
+          service.spec_questions.map(async (questionId, index) => {
+            const item = await findQuestionMongo(
+              service.spec_questions[index].$oid
+            );
+            if (item !== undefined) {
+              const servicePG = await findPGService(service);
+              var questionText;
+
+              const question = {
+                id: questionCount,
+                type: 'question',
+                skip: false,
+                input_bar: false,
+                client_only: false,
+                filter_show: item.filter,
+                pro_page: item.check,
+                is_template: false,
+                createdAt: '2020-12-11 01:23:53.941+02',
+                updatedAt: '2020-12-11 01:23:53.941+02',
+                serviceId: servicePG.id,
+              };
+
+              // console.log(question);
+              questionCount++;
+              questions.push(question);
+              // console.log(questions);
+
+              // // Question Texts START
+
+              // //Pro Question text
+              // questionText = {
+              //   id: questionTextCount,
+              //   uz: item.question.uz,
+              //   ru: item.question.ru,
+              //   eng: item.question.eng,
+              //   belongs: 'pro',
+              //   group_id: 0,
+              //   type: 'question',
+              //   multiple: false,
+              //   createdAt: '2020-12-11 01:23:53.941+02',
+              //   updatedAt: '2020-12-11 01:23:53.941+02',
+              //   questionId: questionCount - 1,
+              // };
+              // questionTextCount++;
+              // //console.log(questionText);
+              // questionTextArray.push(questionText);
+
+              // //Client Question text
+              // questionText = {
+              //   id: questionTextCount,
+              //   uz: item.title.uz,
+              //   ru: item.title.ru,
+              //   eng: item.title.eng,
+              //   belongs: 'client',
+              //   group_id: 0,
+              //   type: 'question',
+              //   multiple: false,
+              //   createdAt: '2020-12-11 01:23:53.941+02',
+              //   updatedAt: '2020-12-11 01:23:53.941+02',
+              //   questionId: questionCount - 1,
+              // };
+              // questionTextCount++;
+              // //console.log(questionText);
+              // questionTextArray.push(questionText);
+
+              // // Choiceses QuestionText
+
+              // await item.values.map((value, index) => {
+              //   questionText = {
+              //     id: questionTextCount,
+              //     uz: value.uz,
+              //     ru: value.ru,
+              //     eng: value.eng,
+              //     belongs: 'client',
+              //     group_id: index,
+              //     type: 'choice',
+              //     multiple: item.multiple,
+              //     createdAt: '2020-12-11 01:23:53.941+02',
+              //     updatedAt: '2020-12-11 01:23:53.941+02',
+              //     questionId: questionCount - 1,
+              //   };
+              //   questionTextCount++;
+              //   //console.log(questionText);
+              //   questionTextArray.push(questionText);
+              // });
+
+              //Question Text END
+            }
+          })
+        );
+      }
       //console.log(questions)
       //console.log(questionTextArray)
-  
     })
-
-    
   ).then(() => {
-    let questionsStringify =  JSON.stringify(questions);
-    let questionsTextStringify =  JSON.stringify(questionTextArray);
-    fs.writeFile('pg-questions.json',questionsStringify,'utf8', function(err) {
-      if (err) throw err;
-      console.log('complete');
+    console.log(questions);
+    let questionsStringify = JSON.stringify(questions);
+    // let questionsTextStringify = JSON.stringify(questionTextArray);
+    fs.writeFile(
+      'pg-questions.json',
+      questionsStringify,
+      'utf8',
+      function (err) {
+        if (err) throw err;
+        console.log('complete');
       }
-  );
-  fs.writeFile('pg-questionsTexts.json', questionsTextStringify,'utf8', function(err) {
-      if (err) throw err;
-      console.log('complete');
-      }
-  );
+    );
+
+    // fs.writeFile(
+    //   'pg-questionsTexts.json',
+    //   questionsTextStringify,
+    //   'utf8',
+    //   function (err) {
+    //     if (err) throw err;
+    //     console.log('complete');
+    //   }
+    // );
   });
 };
 
